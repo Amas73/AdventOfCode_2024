4691124ae0d0fe285bd9118658c9cb799897d9c8
Amas73 <44020327+Amas73@users.noreply.github.com>
2025-01-07 17:17:03 +1000

    Update aoc_12.py

--- aoc_12.py
+++ aoc_12.py
@@ -1,30 +1,45 @@
-from copy import copy
+fn = open('2024\\input_12_test.txt','r')
+#fn = open('2024\\input_12.txt','r')
 
-fn = open('input_12_test.txt','r')
-#fn = open('input_12.txt','r')
-
-data = [[x for x in r.strip('\n')] for r in fn.readlines()]
+data = [r.strip('\n') for r in fn.readlines()]
 w = len(data[0])
 h = len(data)
 
 tot1 = 0
 tot2 = 0
+
+fields = []
+regions = []
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+emptyGrid = [[0 for x in range(w+2)] for y in range(h+2)]
+counted = [[[] for x in range(w)] for y in range(h)]
 
-counted = [[0]*w]*h
-
-def plot(grid, x, y, type):
-  if (x < 0 or x >= w or y < 0 or y >= h or grid[y][x] != type or counted[y][x] > 0):
+def travel(grid, x, y, currType):
+  global region
+  if (x >= 0 and x < w and y >= 0 and y < h and currType in counted[y][x]):
     return
-  if grid[y][x] == type:
-    counted[y][x] = 1
-    
+  if (x < 0 or x >= w or y < 0 or y >= h or grid[y][x] != currType):
+    region[1] +=1
+    region[2][y+1][x+1] = 1
+    return
+  region[0] += 1
+  counted[y][x].append(currType)
   for dx, dy in directions:
-    plot(grid, x + dx, y + dy, type)
+    travel(grid, x + dx, y + dy, currType)
+  return region
+
+for y, row in enumerate(data[:1]):
+  for x, plant in enumerate(row):
+    region = [0,0,emptyGrid[:]]
+    if not counted[y][x]:
+      regions.append(travel(data, x, y, plant))
+
+for x,y,grid in regions[:1]:
+    tot1 += x*y
+    for a in grid:
+      print(a)
 
 
+print (f"Day 10 part 1 value is: {tot1}")
 
-print (f"Day 12 part 1 value is: {tot1}")
-
-print (f"Day 12 part 2 value is: {tot2}")
-
+print (f"Day 10 part 2 value is: {tot2}")
