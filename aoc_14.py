f98aa79f524d8d8f157399e0e312b8fa953b9305
Amas73 <44020327+Amas73@users.noreply.github.com>
2025-01-10 17:27:10 +1000

    Update aoc_14.py

--- aoc_14.py
+++ aoc_14.py
@@ -2,14 +2,19 @@ from pygame import Vector2
 from pygame import Rect
 from copy import deepcopy
 
-fn = open('2024\\input_14_test.txt','r')
-fn = open('2024\\input_14.txt','r')
+runTest = False
+
+if runTest:
+  fn = open('2024\\input_14_test.txt','r')
+  w = 11
+  h = 7
+else:
+  fn = open('2024\\input_14.txt','r')
+  w = 101
+  h = 103
 
 data = [r.strip('\n') for r in fn.readlines()]
 bots = [[v.split(',') for v in a] for a in [[x.split("=")[1] for x in r.split(" ")] for r in data]]
-#grid size
-w = 101
-h = 103
 ebhq = [[0 for x in range(w)] for y in range(h)]
 area = Rect((0,0), (w,h))
 
@@ -20,13 +25,10 @@ for r in bots:
     tmp.append(Vector2(int(x),int(y)))
   robots.append(tmp)
 
-tot1 = 1
-tot2 = 0
-
-def travel(grid, pos, vel, n):
+def travel(pos, vel, n):
+  global time
+  time[100-n][int(pos.y)][int(pos.x)] += 1
   if n == 0:
-    #print(pos)
-    grid[int(pos.y)][int(pos.x)] += 1
     return
   n -= 1
   pos += vel
@@ -39,20 +41,46 @@ def travel(grid, pos, vel, n):
       pos.y += h
     if pos.y >= h:
       pos.y -= h
-  travel(grid, pos, vel, n)
+  travel(pos, vel, n)
 
-finishPos = deepcopy(ebhq)
+def safteyFactor(grid):
+  hw, hh = w // 2, h // 2
+  q1 = sum([sum(r[:hw]) for r in grid[:hh]])
+  q2 = sum([sum(r[hw+1:]) for r in grid[:hh]])
+  q3 = sum([sum(r[:hw]) for r in grid[hh+1:]])
+  q4 = sum([sum(r[hw+1:]) for r in grid[hh+1:]])
+
+  tot = 1
+  for val in [q1,q2,q3,q4]:
+    tot *= val
+  return [tot,[q1,q2,q3,q4]]
+
+def prntGrid(grid):
+  for r in grid:
+    print(r)
+
+time = []
+for n in range(101):
+  time.append(deepcopy(ebhq))
+
 for p, v in robots:
-  travel(finishPos, p, v, 100)
+  travel(p, v, 100)
 
-hw, hh = w // 2, h // 2
-q1 = sum([sum(r[:hw]) for r in finishPos[:hh]])
-q2 = sum([sum(r[hw+1:]) for r in finishPos[:hh]])
-q3 = sum([sum(r[:hw]) for r in finishPos[hh+1:]])
-q4 = sum([sum(r[hw+1:]) for r in finishPos[hh+1:]])
+tot1 = safteyFactor(time[100])[0]
 
-for val in [q1,q2,q3,q4]:
-  tot1 *= val
+tot2 = 0
+tmp = 0
+for x in range(100):
+  # t = safteyFactor(time[x])[1]
+  # top = t[0] + t[1]
+  # bot = t[2] + t[3]
+  # if bot > (top*2.5):
+  #   #tmp = t
+  #   tot2 = x
+  #   break
+  print(f"\n\n   ----- for time of {x} seconds:")
+  prntGrid(time[x])
+  input()
 
 print (f"Day 14 part 1 value is: {tot1}")
 print (f"Day 14 part 2 value is: {tot2}")
